c9aa9ab4124cbafe9d5846071a920b89197f20bd
Artur Stambultsian <bumbledos@gmail.com>
2019-11-28 18:31:10 +0300

    added ignoreKeywords for scale-unlimited/declaration-strict-value rule

--- index.js
+++ index.js
@@ -30,13 +30,19 @@ module.exports = {
       { ignore: ['after-comment'], except: ['first-nested', 'after-same-name'] },
     ],
 
-    'scale-unlimited/declaration-strict-value': [
+    'scale-unlimited/declaration-strict-value': [[
       'background-image',
       'background',
       '/color/',
       'fill',
       'stroke',
-    ],
+    ], {
+      ignoreKeywords: [
+        'currentColor',
+        'transparent',
+        'inherit',
+      ],
+    }],
     'max-empty-lines': 1,
     'declaration-no-important': true,
     'no-duplicate-selectors': true,
